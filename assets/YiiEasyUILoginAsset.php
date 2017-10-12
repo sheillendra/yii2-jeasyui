c5f1447cda51dc90ad30fe3ba537b160424eb7ed
sheillendra <sheillendra@yahoo.com>
2017-10-12 20:57:16 +0700

    prepare v.2.0.1-alpha

--- assets/YiiEasyUILoginAsset.php
+++ assets/YiiEasyUILoginAsset.php
@@ -12,7 +12,7 @@ use yii\web\AssetBundle;
 
 class YiiEasyUILoginAsset extends AssetBundle {
 
-    public $sourcePath = '@sheillendra/jeasyui/assets/dist/v2.0.0-alpha';
+    public $sourcePath = '@sheillendra/jeasyui/assets/dist/v2.0.1-alpha';
     public $css = ['css/yii.easyui-login.css'];
     public $js = ['js/yii.easyui-login.min.js'];
     public $depends = [
