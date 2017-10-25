ae8f28f37be4cc3dba839727c8efe618376b2ba2
sheillendra <sheillendra@yahoo.com>
2017-10-26 06:47:25 +0700

    add c3 chart

---
+++ assets/C3Asset.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @link http://www.sheillendra.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace sheillendra\jeasyui\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Sheillendra <sheillendra@yahoo.com>
+ * @since 2.0
+ */
+class C3Asset extends AssetBundle {
+
+    public $sourcePath = '@bower/c3';
+    public $css = [
+        'c3.min.css',
+    ];
+    public $js = [
+        'c3.min.js'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+    public $publishOptions = [
+        'only' => [
+            'c3.min.css', 'c3.min.js'
+        ]
+    ];
+}
