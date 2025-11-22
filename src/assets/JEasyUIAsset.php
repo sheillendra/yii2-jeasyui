6affef913b08b65a928cafe8c2b714bf09749c81
sheillendra <suryana689@gmail.com>
2025-11-22 19:36:11 +1000

    change to new version of jeasyui

--- src/assets/JEasyUIAsset.php
+++ src/assets/JEasyUIAsset.php
@@ -17,7 +17,7 @@ use yii\web\AssetBundle;
 
 class JEasyUIAsset extends AssetBundle
 {
-    public $sourcePath = '@sheillendra/jeasyui/assets/jquery-easyui-1.11.3';
+    public $sourcePath = '@sheillendra/jeasyui/assets/jquery-easyui-1.11.4';
     public $css = [
         'themes/default/easyui.css',
         'themes/icon.css',
